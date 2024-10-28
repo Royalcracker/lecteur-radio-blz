81030bedbdab29656d750a1a2bd8aa34ee2c5510
SPRIET Valentin <valentin.spriet@interieur.gouv.fr>
2024-10-28 11:25:23 +0100

    test

--- src/components/Radio.vue
+++ src/components/Radio.vue
@@ -5,11 +5,11 @@
 
 
   async function getRadioData() {
-    const response = await fetch("https://cast.blzradio.fr/status-json.xsl")
-    // const response = await fetch("https://blzradio.fr:8443/status-json.xsl")
+    // const response = await fetch("https://cast.blzradio.fr/status-json.xsl")
+    const response = await fetch("https://blzradio.fr:8443/status-json.xsl")
     const json = await response.json();
-    urlRadio.value = "https://cast.blzradio.fr/radio1"
-    // urlRadio.value = json.icestats.source.listenurl
+    // urlRadio.value = "https://cast.blzradio.fr/radio1"
+    urlRadio.value = json.icestats.source.listenurl
     titleRadio.value = json.icestats.source.title
   }
 
